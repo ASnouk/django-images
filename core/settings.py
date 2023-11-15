f01fb09671ae697bc9617f227de11bc2e44b692a
ASnouk <89106867+ASnouk@users.noreply.github.com>
2023-11-15 13:44:40 +0200

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -146,8 +146,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DEFAULT_FILE_STORAGE = 'core.azure_storage.AzureMediaStorage'
 STATICFILES_STORAGE = 'core.azure_storage.AzureStaticStorage'
 
-AZURE_ACCOUNT_NAME = os.getenv('snouk')
-AZURE_ACCOUNT_KEY = os.getenv('n78aMek/MNkyxJ7fa5FsmlYl4OCZYQ5aL35aOuVSgqRyxqsrF/SonWh6k1cHboj/3lmr0U5Kw7EP+AStYNQINQ==')
+AZURE_ACCOUNT_NAME = os.getenv('AZURE_ACCOUNT_NAME')
+AZURE_ACCOUNT_KEY = os.getenv('AZURE_ACCOUNT_KEY')
 AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
 
 STATIC_URL = f'https://{AZURE_CUSTOM_DOMAIN}/static/'
